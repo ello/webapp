6f59d01fe38e0684271bce87685a4394a0463d9b
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-12-13 15:55:13 -0700

    Remove consoles and fix the router history issue

--- src/containers/HeroContainer.js
+++ src/containers/HeroContainer.js
@@ -83,7 +83,6 @@ function mapStateToProps(state, props) {
     promotions = categoryData.promotionals
   }
   return {
-    authPromotionals: selectAuthPromotionals(state),
     broadcast: selectBroadcast(state),
     categoryData,
     dpi: selectDPI(state),
@@ -150,8 +149,6 @@ class HeroContainer extends Component {
         keyArr.push(key)
       })
       const randomKey = sample(keyArr)
-      console.log('keyArr', keyArr)
-      console.log('promotion randomKey', randomKey)
       this.setState({ promotion: promotions.get(randomKey) || Immutable.Map() })
     }
     if (broadcast !== this.state.broadcast) {
