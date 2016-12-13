8ef386df7b0a19b8d7f69eb4948450c800ae69fc
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-12-13 11:43:03 -0700

    Remove react shallow compare from the project

--- src/containers/SignUpContainer.js
+++ src/containers/SignUpContainer.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import shallowCompare from 'react-addons-shallow-compare'
 import { MainView } from '../components/views/MainView'
 import RegistrationRequestForm from '../components/forms/RegistrationRequestForm'
 
 export default class SignUpContainer extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return shallowCompare(this, nextProps, nextState)
+  shouldComponentUpdate() {
+    return true
   }
 
   render() {
