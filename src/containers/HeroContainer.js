8b1b701a0214f7a514762f68b1ed663e202d78c1
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-12-19 14:34:11 -0700

    Add back in the persistance layer

--- src/containers/HeroContainer.js
+++ src/containers/HeroContainer.js
@@ -23,6 +23,7 @@ import {
   selectPagePromotionals,
 } from '../selectors/promotions'
 import { selectPathname, selectViewNameFromRoute } from '../selectors/routing'
+import { selectJson } from '../selectors/store'
 import { selectUserFromUsername } from '../selectors/user'
 import { trackEvent } from '../actions/analytics'
 import {
@@ -41,7 +42,6 @@ import {
   HeroPromotionPage,
 } from '../components/heros/HeroRenderables'
 
-const selectJson = state => state.get('json')
 export const selectIsAuthentication = createSelector(
   [selectViewNameFromRoute], viewName => viewName === 'authentication',
 )
