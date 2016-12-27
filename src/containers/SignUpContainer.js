217629a1805766876416ed57d4b80e59b5bc8193
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-12-27 16:42:19 -0700

    Adds back in the shouldComponentUpdate methods

--- src/containers/SignUpContainer.js
+++ src/containers/SignUpContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import shallowCompare from 'react-addons-shallow-compare'
 import { MainView } from '../components/views/MainView'
 import RegistrationRequestForm from '../components/forms/RegistrationRequestForm'
 
 export default class SignUpContainer extends Component {
-  shouldComponentUpdate() {
-    return true
+  shouldComponentUpdate(nextProps, nextState) {
+    return shallowCompare(this, nextProps, nextState)
   }
 
   render() {
