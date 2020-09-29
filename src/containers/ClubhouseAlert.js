ba64901d36c73879a11ee989152acf9495510672
Colin T.A. Gray <colinta@gmail.com>
2020-09-29 14:32:37 -0400

    clubhouse promo

--- src/containers/ClubhouseAlert.js
+++ src/containers/ClubhouseAlert.js
@@ -5,6 +5,8 @@ import { closeClubhouseAlert } from '../actions/gui'
 import { closeModal } from '../actions/modals'
 import { css, media, descendent } from '../styles/jss'
 
+const chLogo = '/static/images/ch-logo.png'
+
 export function mapStateToProps() {
   return {
   }
@@ -16,13 +18,18 @@ const clubhouseAlertStyle = css(
     top: '50%',
     margin: 'auto',
     transform: 'translateX(50%) translateY(-50%)',
-    background: 'rgba(0,0,0,0.9)',
+    background: 'rgba(245, 245, 245, 0.9)',
     padding: '48px',
     maxWidth: '30rem',
     zIndex: '9999',
   },
   descendent('h2,p', {
-    color: 'white',
+    color: 'black',
+  }),
+  descendent('h2,img', {
+    display: 'inline-block',
+    height: '60px',
+    marginTop: '-2px',
   }),
   descendent('h2', {
     fontSize: '1.5rem',
@@ -42,14 +49,12 @@ const clubhouseAlertStyle = css(
     lineHeight: '3.75rem',
     paddingRight: '1.875rem',
     paddingLeft: '1.875rem',
-    fontSize: '0.875rem',
     color: 'rgb(255, 255, 255)',
-    backgroundColor: 'rgb(0, 209, 0)',
-    borderRadius: '0.3125rem',
-    borderWidth: '1px',
-    borderStyle: 'solid',
-    borderColor: 'rgb(0, 209, 0)',
-    borderImage: 'initial',
+    backgroundColor: 'rgb(196,92,221)',
+    background: 'linear-gradient(0deg, rgba(196,92,221,1) 0%, rgba(79,109,253,1) 100%)',
+    fontSize: '1rem',
+    fontWeight: 'bold',
+    border: 'none',
     transition: 'background-color 0.2s cubic-bezier(0.23, 1, 0.32, 1) 0s, border-color 0.2s cubic-bezier(0.23, 1, 0.32, 1) 0s, color 0.2s cubic-bezier(0.23, 1, 0.32, 1) 0s, width 0.2s cubic-bezier(0.23, 1, 0.32, 1) 0s',
   }),
   descendent('button.main:hover, button.main:focus, button.main:active', {
@@ -58,6 +63,9 @@ const clubhouseAlertStyle = css(
     borderColor: 'rgb(0, 177, 0)',
   }),
 
+  descendent('a', {
+    color: '#000',
+  }),
   descendent('button', {
     background: 'transparent',
     border: '0',
@@ -91,7 +99,7 @@ class ClubhouseAlert extends PureComponent {
   }
 
   onClickAccept = () => {
-    const redirectUrl = 'https://tlnt.at/2G4EMzy'
+    const redirectUrl = 'https://tlnt.at/30lF38W'
     const win = window.open(redirectUrl, '_blank')
     win.focus()
     this.closeModalAndAlert()
@@ -111,7 +119,7 @@ class ClubhouseAlert extends PureComponent {
     return (
       <div className={clubhouseAlertStyle}>
         <h2>
-          Let&rsquo;s build a more creative world with Club.House
+          Let&rsquo;s build a more creative world with <img src={chLogo} alt="Club.House" />
         </h2>
         <p>
           Club.House is a place for Young Creators to learn and collaborate with brands,
@@ -119,7 +127,7 @@ class ClubhouseAlert extends PureComponent {
         </p>
         <div className="actions">
           <button className="main" onClick={this.onClickAccept}>Join the Club</button>
-          <button className="main" onClick={this.onClickClose}>No Thanks</button>
+          <a href="#nothanks" onClick={this.onClickClose}>No Thanks</a>
         </div>
       </div>)
   }
